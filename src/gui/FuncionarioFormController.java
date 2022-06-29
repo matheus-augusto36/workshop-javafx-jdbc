4b4b2d40788ada7ed7a23e211e88a7effe7f27d8
matheus-augusto36 <silvamatheusaugusto36@gmail.com>
2022-06-29 17:23:15 -0300

    Revert "Revert "projeto criado""
    This reverts commit 87ea9216844a12370497f51cbec1f5c2583380db.

--- src/gui/FuncionarioFormController.java
+++ src/gui/FuncionarioFormController.java
@@ -1,44 +1,45 @@
 package gui;
 
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 import java.util.ResourceBundle;
 import java.util.Set;
 
-import db.DbException;
-import gui.listeners.DataChangeListener;
-import gui.util.Alerts;
 import gui.util.Constraints;
 import gui.util.Utils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import model.entities.Department;
+import model.entities.Funcionario;
 import model.exceptions.ValidationException;
-import model.services.DepartmentService;
+import model.services.FuncionarioService;
 
-public class DepartmentFormController implements Initializable {
-
-	private List<DataChangeListener> dataChangeListeners = new ArrayList<>();
+public class FuncionarioFormController implements Initializable {
 	
-	private DepartmentService service;
+	private FuncionarioService service;
 	
-	private Department entity;
+	private Funcionario entity;
 	
 	@FXML
-	private TextField txtId;
+	private TextField txtEmail;
 	
 	@FXML 
 	private TextField txtName;
 	
 	@FXML
+	private TextField txtArea;
+	
+	@FXML
 	private Label labelErrorName;
+	
+	@FXML
+	private Label labelErrorEmail;
+	
+	@FXML
+	private Label labelErrorArea;
 	
 	@FXML 
 	private Button btSave;
@@ -46,16 +47,16 @@ public class DepartmentFormController implements Initializable {
 	@FXML 
 	private Button btCancel;
 	
-	public void setDepartment(Department entity) {
+	public void setFuncionario(Funcionario entity) {
 		this.entity = entity;
 	}
 	
-	public void setDepartmentService(DepartmentService service) {
+	public void setFuncionarioService(FuncionarioService service) {
 		this.service = service;
 	}
 	
-	public void subscribeDataChangeListener(DataChangeListener listener) {
-		dataChangeListeners.add(listener);
+	public FuncionarioService getFuncionarioService() {
+		return service;
 	}
 	
 	@FXML
@@ -63,39 +64,51 @@ public class DepartmentFormController implements Initializable {
 		if(entity == null) {
 			throw new IllegalStateException("Entity is null!");
 		}
+		
 		if(service == null) {
 			throw new IllegalStateException("Service is null");
 		}
+		
+		if(labelErrorName != null) {
+			labelErrorName.setText(null);
+		}
+		
+		if(labelErrorEmail != null) {
+			labelErrorEmail.setText(null);
+		}
+		
+		if(labelErrorArea != null) {
+			labelErrorArea.setText(null);
+		}
+		
 		try {
 			entity = getFormData(); //pega os dados dos TextField e seta os atributos de entity com esses dados(inverso de updateFormData()).
-			service.saveOrUpdate(entity); //se o departamento ja existir, atualiza. Se n�o, cria um novo.
-			notifyDataChangeListeners(); //avisa os "listeners"
+			service.saveOrUpdate(entity); //implementar!!!
 			Utils.currentStage(event).close(); //fecha a tela.
-		} catch (DbException e) {
-			Alerts.showAlert("Error saving object", null, e.getMessage(), AlertType.ERROR);
 		} catch (ValidationException e) {
 			setErrorMessages(e.getErrors());
 		}
 	}
-	
-	private void notifyDataChangeListeners() {
-		for(DataChangeListener listener: dataChangeListeners) {
-			listener.onDataChanged();
-		}
-		
-	}
 
-	private Department getFormData() {
-		Department obj = new Department();
+	private Funcionario getFormData() {
+		Funcionario obj = new Funcionario();
 		
-		ValidationException exception = new ValidationException("Validation error!");
-		
-		obj.setId(Utils.tryParseToInt(txtId.getText()));
+		ValidationException exception = new ValidationException("Erro de validação");
 		
 		if(txtName.getText() == null || txtName.getText().trim().equals(" ")) {
 			exception.addError("name", " Field can't be empty");
 		}
 		obj.setName(txtName.getText());
+		
+		if(txtEmail.getText() == null || txtEmail.getText().trim().equals(" ")) {
+			exception.addError("email", " Field can't be empty");
+		}
+		obj.setEmail(txtEmail.getText());
+		
+		if(txtArea.getText() == null || txtArea.getText().trim().equals(" ")) {
+			exception.addError("area", " Field can't be empty");
+		}
+		obj.setArea(txtArea.getText());
 		
 		if(exception.getErrors().size() > 0) {
 			throw exception;
@@ -109,23 +122,32 @@ public class DepartmentFormController implements Initializable {
 	}
 	
 	private void InitializeNodes() {
-		Constraints.setTextFieldInteger(txtId);
-		Constraints.setTextFieldMaxLength(txtName, 30);
+		Constraints.setTextFieldMaxLength(txtEmail, 40);
+		Constraints.setTextFieldMaxLength(txtName, 40);
 	}
 	
 	public void updateFormData() {
 		if(entity == null) {
 			throw new IllegalStateException("Entity is null!");
 		}
-		txtId.setText(String.valueOf(entity.getId()));
+		txtEmail.setText(entity.getEmail());
 		txtName.setText(entity.getName());
+		txtArea.setText(entity.getArea());
 	}
 	
-	private void setErrorMessages(Map<String, String> errors) {
+	private void setErrorMessages(Map<String, String> errors) { 
 		Set<String> fields = errors.keySet();
 		
 		if(fields.contains("name")) {
 			labelErrorName.setText(errors.get("name"));
+		}
+		
+		if(fields.contains("email")) {
+			labelErrorEmail.setText(errors.get("email"));
+		}
+		
+		if(fields.contains("area")) {
+			labelErrorArea.setText(errors.get("area"));
 		}
 	}
 	
--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -3,77 +3,60 @@ package gui;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
-import java.util.function.Consumer;
 
-import application.Main;
 import gui.util.Alerts;
+import gui.util.Utils;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert.AlertType;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.layout.VBox;
-import model.services.DepartmentService;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import model.entities.Funcionario;
+import model.services.FuncionarioService;
 
 public class MainViewController implements Initializable {
-
-	@FXML
-	private MenuItem menuItemSeller;
 	
 	@FXML
-	private MenuItem menuItemDepartment;
+	private Button btNovo;
 	
 	@FXML
-	private MenuItem menuItemAbout;
-	
-	@FXML
-	public void onMenuItemSellerAction() {
-		System.out.println("onMenuItemSellerAction");
-	}
-	
-	@FXML
-	public void onMenuItemDepartmentAction() {
-		loadView("/gui/DepartmentList.fxml", (DepartmentListController controller) -> {
-			controller.setDepartmentService(new DepartmentService());
-			controller.updateTableView();
-		});
-	}
-	
-	@FXML
-	public void onMenuItemAboutAction() {
-		loadView("/gui/About.fxml", x -> {});
+	private void onBtNewAction(ActionEvent event) {
+		Stage currentStage = Utils.currentStage(event);
+		Funcionario obj = new Funcionario();
+		createDialogForm("/gui/FuncionarioForm.fxml", currentStage, obj);
 	}
 	
 	@Override
-	public void initialize(URL url, ResourceBundle rb) {
-		
+	public void initialize(URL url, ResourceBundle rb) {	
 		
 	}
 	
-	private synchronized <T> void loadView(String absoluteName, Consumer<T> initializingAction) {
+	private void createDialogForm(String absoluteName, Stage parentStage, Funcionario obj) {
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
-			VBox newVbox = loader.load();
+			Pane pane = loader.load();
 			
-			Scene mainScene = Main.getMainScene();
-			VBox mainVBox = (VBox)((ScrollPane)mainScene.getRoot()).getContent();
+			FuncionarioFormController controller = loader.getController();
+			controller.setFuncionario(obj);
+			controller.setFuncionarioService(new FuncionarioService());
+			controller.updateFormData();
 			
-			Node mainMenu = mainVBox.getChildren().get(0);
-			mainVBox.getChildren().clear();
-			mainVBox.getChildren().add(mainMenu);
-			mainVBox.getChildren().addAll(newVbox.getChildren());
-			
-			T controller = loader.getController(); //pega o controlador mencionado na expressão lambda
-			initializingAction.accept(controller); //executa a expressão usando o "controller" como referência
+			Stage dialogStage = new Stage();
+			dialogStage.setTitle("Entrada de dados do funcionario");
+			dialogStage.setScene(new Scene(pane));
+			dialogStage.setResizable(false);
+			dialogStage.initOwner(parentStage);
+			dialogStage.initModality(Modality.WINDOW_MODAL);
+			dialogStage.showAndWait();
 			
 		} catch (IOException e) {
-			Alerts.showAlert("IO Exception", "Error loading page", e.getMessage(), AlertType.ERROR);
+			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
 		}
 	}
-	
-
 
 }
